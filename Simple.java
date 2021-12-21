0436331452c9afb4c99011005c6d5fce580cbb12
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-21 11:11:45 +0530

    lol

---
+++ Simple.java
@@ -0,0 +1,15 @@
+class Simple{
+	public static void main(String []args){
+		short[] rollno = {10,12,13,14,15,16,17,18,19,20,21,22};
+		System.out.println(rollno[0]);
+		System.out.println(rollno[2]);
+		System.out.println(rollno[3]);
+		System.out.println(rollno[1]);
+		System.out.println(rollno[4]);
+		System.out.println(rollno[5]);
+		System.out.println(rollno[6]);
+		System.out.println(rollno[7]);
+		System.out.println(rollno[8]);
+		System.out.println(rollno[9]);
+	}
+}
